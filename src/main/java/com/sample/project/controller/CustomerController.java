35305f1f7243abd1d5d0c246ef5fe06141e9e9ae
FinKim <khr7779@gmail.com>
2023-11-28 22:56:30 +0900

    list show

--- src/main/java/com/sample/project/controller/CustomerController.java
+++ src/main/java/com/sample/project/controller/CustomerController.java
@@ -1,14 +1,22 @@
 package com.sample.project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.sample.project.service.CustomerService;
 
 @Controller
 public class CustomerController {
 
-	@GetMapping("/home")
-	public String home() {
-		return "home"; // This refers to home.html in the templates directory
+	@Autowired
+	private CustomerService customerService;
+
+	@GetMapping("/customers")
+	public String listCustomers(Model model) {
+		model.addAttribute("customers", customerService.getAllCustomers());
+		return "customers";
 	}
 
 }
--- src/main/java/com/sample/project/service/CustomerService.java
+++ src/main/java/com/sample/project/service/CustomerService.java
@@ -1,14 +1,25 @@
 package com.sample.project.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sample.project.dto.CustomerRepository;
+import com.sample.project.entity.Customer;
 
 @Service
 public class CustomerService {
 
 	@Autowired
 	private CustomerRepository customerRepository;
+
+	public List<Customer> getAllCustomers() {
+		return customerRepository.findAll();
+	}
+
+	public void saveCustomer(Customer customer) {
+		customerRepository.save(customer);
+	}
 
 }
